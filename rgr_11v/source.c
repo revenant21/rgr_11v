791d6e1ed6808edb6a3db98f587df14e0b779586
revenant21 <skyt9755@gmail.com>
2025-11-22 17:15:15 +0600

    minor update 1. добавлена функция для определения минимума/максимума у определенной функции 2. небольшие изменения в меню

--- rgr_11v/source.c
+++ rgr_11v/source.c
@@ -11,17 +11,24 @@
 #define KEY_ESC 27
 
 #define MENU_SIZE 7
+
+//specs of math functions
 #define a 0.00
 #define b 2 * 3.1415
 #define n 20
 
+//colors for max/min
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_RESET   "\x1b[0m"
+
 const char* menu_items[MENU_SIZE] = {
-    "1. Автор",
-    "2. Заставка",
-    "3. Таблица",
-    "4. Уравнение",
-    "5. Интеграл",
-    "6. График",
+    "1. Таблица",
+    "2. Графики",
+    "3. Уравнения",
+    "4. Интеграл",
+    "5. Заставка",
+    "6. Об авторе",
     "7. Выход"
 };
 
@@ -45,22 +52,105 @@ void HideCursor() {
     SetConsoleCursorInfo(consoleHandle, &info);
 }
 
+//function for pre-solve
+void solve_func(double res[3][n])
+{
+    double step = fabs(b - a) / (n - 1);
+
+    res[0][0] = a;
+
+    for (int num = 0; num < n; num++)
+    {
+        res[1][num] = 5 - 3 * cos(res[0][num]);
+        res[2][num] = sqrt(1 + pow(sin(res[0][num]), 2));
+        res[0][num + 1] = res[0][num] + step;
+    }
+}
+
 // draw table
 void table_draw()
 {
-    double F1, F2, s = fabs(b - a) / (n - 1), x = a;
+    double res[3][n];
+    int min1, min2, max1, max2;
 
     printf(" ___________________________________________ \n");
     printf(" | I |     X    |     F1     |      F2     | \n");
     printf(" |---|----------|------------|-------------|\n");
+    solve_func(res);
+
+    min1 = min_table(res, 1);
+    min2 = min_table(res, 2);
+    max1 = max_table(res, 1);
+    max2 = max_table(res, 2);
+
+    //check for min/max and coloring
     for (int i = 0; i < n; i++)
     {
-        F1 = 5 - 3 * cos(x);
-        F2 = sqrt(1 + pow(sin(x), 2));
-        printf(" |%3d| %8.3f |%12.7f| %12.8f|\n", i + 1, x, F1, F2);
-        x += s;
+        printf(" |%3d| %8.3f |", i + 1, res[0][i]);
+        
+        if (i == min1)
+        {
+            printf(COLOR_RED);
+        }
+        else if (i == max1)
+        {
+            printf(COLOR_GREEN);
+        }
+        
+        printf("%12.7f", res[1][i]);
+        printf(COLOR_RESET);
+        printf("|");
+
+        if (i == min2)
+        {
+            printf(COLOR_RED);
+        }
+        else if (i == max2)
+        {
+            printf(COLOR_GREEN);
+        }
+
+        printf(" %12.8f", res[2][i]);
+        printf(COLOR_RESET);
+        printf("|\n");
+
+        //printf(" |%3d| %8.3f |%12.7f| %12.8f|\n", i + 1, res[0][i], res[1][i], res[2][i]);
     }
     printf(" ------------------------------------------- \n");
+    printf("Легенда: " COLOR_GREEN "Максимум " COLOR_RED "Минимум" COLOR_RESET);
+}
+
+//min and max for table_draw()
+int min_table(double res[3][n], int indx) 
+{
+    int result;
+    double min = pow(10, 5);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (res[indx][i] < min)
+        {
+            min = res[indx][i];
+            result = i;
+        }
+    }
+    return result;
+}
+
+int max_table(double res[3][n], int indx)
+{
+    int result;
+    double max = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (res[indx][i] > max)
+        {
+            max = res[indx][i];
+            result = i;
+        }
+    }
+    return result;
 }
 
 int main() {
@@ -115,24 +205,24 @@ int main() {
             switch (selected)
             {
             case 0:
-                printf("\n> Выбран пункт: Автор\n");
-                printf("> Автор: ст.гр. ИВТ-254 Дубровин А.А.\n");
-                break;
-            case 1:
-                printf("\n> Выбран пункт: Заставка\n");
-                break;
-            case 2:
                 printf("\n> Таблица значений:\n");
                 table_draw();
                 break;
-            case 3:
+            case 1:
+                printf("\n> Выбран пункт: Графики\n");
+                break;
+            case 2:
                 printf("\n> Выбран пункт: Уравнение\n");
                 break;
-            case 4:
+            case 3:
                 printf("\n> Выбран пункт: Интеграл\n");
                 break;
+            case 4:
+                printf("\n> Выбран пункт: Заставка\n");
+                break;
             case 5:
-                printf("\n> Выбран пункт: График\n");
+                printf("\n> Выбран пункт: Автор\n");
+                printf("> Автор: ст.гр. ИВТ-254 Дубровин А.А.\n");
                 break;
             case 6:
                 printf("\n> Выход из программы...\n");
